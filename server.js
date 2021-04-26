afe4bb900f89a8841ea6c364c7fdddb3456f9081
Coyls <76538870+Coyls@users.noreply.github.com>
2021-04-26 16:06:11 +0200

    add : backEndTest

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/backEndTest/index.html');
 });
 
-http.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
-});
+
+
+
+
+
+
+
+
+if (port === 3000) {
+  http.listen(port, () => {
+    console.log(`http://localhost:${port}/`);
+  });
+}
+
 
 
 
