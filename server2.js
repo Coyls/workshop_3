b92e85a0f33956bc474eadbdff900139f3df316a
Roxane-gll <Administrateur@CINQ-B15_.formationara.local>
2021-04-27 10:05:56 +0200

    test connection bdd

--- server2.js
+++ server2.js
@@ -2,13 +2,30 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
+const mysql = require('mysql');
 
 app.use("/public", express.static('public'))
 
 app.get('/', (req, res) => {
 
   res.sendFile(__dirname + '/public/back_end_test/index.html');
+});
+
+const con = mysql.createConnection({
+  host: "http://vps215076.ovh.net/",
+  user: "pma",
+  password: "pmapass",
+  database : "comment_ca_va"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+  con.query("SELECT * FROM posts LEFT JOIN reponses ON post.id_reponse=reponse.id_reponse LEFT JOIN mood ON reponse.id_mood=mood.id_mood LEFT JOIN questions ON reponse.id_question=question.id_question LEFT JOIN screen ON question.id_screen=screen.id_screen", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
 
 io.on('connection', (socket) =>{
@@ -18,6 +35,7 @@ io.on('connection', (socket) =>{
   socket.on('message', (message) =>{
     console.log("Le message est : " + message)
   })
+
 
   socket.on('disconnect', () => {
     console.log('disconnect', socket.id)
