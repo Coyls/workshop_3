45ea836953c8dc862bd61e9bd18124e18306edc3
unknown <Administrateur@CINQ-B15_.formationara.local>
2021-04-27 09:55:48 +0200

    creation serveur test sql

---
+++ server2.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000; 
+
+app.use("/public", express.static('public'))
+
+app.get('/', (req, res) => {
+
+  res.sendFile(__dirname + '/public/back_end_test/index.html');
+});
+
+io.on('connection', (socket) =>{
+  console.log('connect', socket.id)
+
+
+  socket.on('message', (message) =>{
+    console.log("Le message est : " + message)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('disconnect', socket.id)
+  })
+}) 
+
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const WebSocket = require('ws')
+ 
+const wss = new WebSocket.Server({ port: 3000 })
+ 
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+}) */
