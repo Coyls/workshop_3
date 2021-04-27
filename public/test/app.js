88ca65ff8d82a096eecf70eed38a855836034d94
Coyls <76538870+Coyls@users.noreply.github.com>
2021-04-27 14:21:34 +0200

    reset

--- public/test/app.js
+++ public/test/app.js
@@ -1,9 +1,29 @@
-const socket = io('ws://vps215076.ovh.net:3000');
-
 console.log("test dossier public")
 
+const url = 'ws://vps215076.ovh.net:3000'
+const connection = new WebSocket(url)
 
 
+let msg = {
+    type: "message",
+    text: "Salut le test",
+    date: Date.now()
+  };
 
 
+// Connection WebSocket
+connection.onopen = (e) => {
+    console.log("e", e)
+    connection.send(JSON.stringify(msg))
 
+    // connection.emit('test', JSON.stringify(msg))
+
+}
+connection.onerror = (error) => {
+    console.log(`WebSocket error: ${error}`)
+}
+
+
+connection.onmessage = (e) => {
+    console.log(e.data)
+}
--- server.js
+++ server.js
@@ -2,32 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const port = process.env.PORT || 3000;
+const WebSocket = require('ws');
 const mysql = require('mysql');
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-});
-
 const server = http.createServer(app);
-const io = require("socket.io")(server);
 
+const wss = new WebSocket.Server({ server })
 
-
-
+const test = 'Une data'
  
-
-
-
+wss.on('connection', ws => {
+  ws.on('test', msg => {
+    console.log("Message recu :", msg)
+  })
+  ws.send(test)
+})
 
 
 
