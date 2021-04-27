d6b49c674da3f9d3b8c753ebfb6e93515b822bd0
Coyls <76538870+Coyls@users.noreply.github.com>
2021-04-27 10:54:02 +0200

    add : mysql and npm

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
+const mysql = require('mysql');
  
 const wss = new WebSocket.Server({ port: 3000 })
  
@@ -8,3 +9,19 @@ wss.on('connection', ws => {
   })
   ws.send('Hello! Message From Server!!')
 })
+
+const con = mysql.createConnection({
+  host: "http://vps215076.ovh.net/",
+  user: "pma",
+  password: "pmapass",
+  database : "comment_ca_va"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+  con.query("SELECT * FROM posts", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
