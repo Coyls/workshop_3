f8b3aa6f0df182a063887c0dceea457a03645a40
Roxane-gll <Administrateur@CINQ-B15_.formationara.local>
2021-04-27 10:29:08 +0200

    test connection bdd 6

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
+const mysql = require('mysql');
 
 app.use("/public", express.static('public'))
 
@@ -18,8 +19,23 @@ io.on('connection', (socket) =>{
   socket.on('disconnect', () => {
     console.log('disconnect', socket.id)
   })
-}) 
+})
 
+const con = mysql.createConnection({
+  host: "http://vps215076.ovh.net/",
+  user: "pma",
+  password: "pmapass",
+  database : "comment_ca_va"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+  con.query("SELECT * FROM posts LEFT JOIN reponses ON post.id_reponse=reponse.id_reponse LEFT JOIN mood ON reponse.id_mood=mood.id_mood LEFT JOIN questions ON reponse.id_question=question.id_question LEFT JOIN screen ON question.id_screen=screen.id_screen", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
