d0c5f8807d96c244a8910a09541ef8166bdd11b6
Coyls <76538870+Coyls@users.noreply.github.com>
2021-04-27 11:26:47 +0200

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const http = require('http');
+const http = require('http');
 const port = process.env.PORT || 3000;
 const WebSocket = require('ws');
 const mysql = require('mysql');
