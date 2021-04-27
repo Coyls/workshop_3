089c13f4c0ef324d1e5e66ca3a6226d501310465
Roxane-gll <Administrateur@CINQ-B15_.formationara.local>
2021-04-27 10:17:12 +0200

    test connection bdd 3

--- server2.js
+++ server2.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3001;
+const port = 3001;
 const mysql = require('mysql');
 
 app.use("/public", express.static('public'))
