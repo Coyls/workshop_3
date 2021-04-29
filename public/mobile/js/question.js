080d59e069426a42b8a8dcd87b8e08251b686f89
unknown <Administrateur@CINQ-B20.formationara.local>
2021-04-29 16:04:48 +0200

    element in fonction

--- public/mobile/js/question.js
+++ public/mobile/js/question.js
@@ -40,11 +40,13 @@ serveurSocket.onerror = (error) => {
 // message ----------------------------------------
 serveurSocket.onmessage = (event) => {
     const data = JSON.parse(event.data)
-
+    console.log("construc Element")
     if (data.type === 'mobileData') {
         console.log("question/response", data.question)
-        idResponseP =data.question[0].reponseId
-        idResponseN=data.question[1].reponseId
+        idResponseP =data.question[0].responseID
+        idResponseN=data.question[1].responseID
+        let format=data.format
+
         document.querySelector("body").innerHTML += `
 <div id="previewRep1"></div>
 <div id="previewRep2"></div>
@@ -65,6 +67,9 @@ serveurSocket.onmessage = (event) => {
     </div>
     <div id="repD-wrapper"><p>${data.question[1].reponses}</p></div>
 </div>`
+
+        console.log('end construct')
+        load_element_slide()
     } else if (data.type === 'mobileState') {
         console.log("State", data.state)
         if (data.state === "inactif") {
@@ -125,88 +130,95 @@ const postMood = (mood,response) => {
 let mov = false
 
 const menu = document.querySelector("#menu-btn")
-
+/*
 window.addEventListener("load", () => {
     if (!wait) {
         setTimeout(() => {
-            const waitingWrapper = document.querySelector("#waiting-wrapper")
-            const questionAndRepWrapper = document.querySelector("#questionAndRep-wrapper")
-            const previewRep1 = document.querySelector("#previewRep1")
-            const previewRep2 = document.querySelector("#previewRep2")
-            console.log("inside timeout");
-            menu.style = "opacity : 0;";
-            waitingWrapper.style = "display : none"
-            questionAndRepWrapper.style = "display : flex;"
-            let swipedir, startY, distY;
 
-            questionAndRepWrapper.addEventListener('touchstart', (e) => {
-                if (!mov) {
-                    swipedir = 'none'
-                    distY = 0
-                    startY = e.changedTouches[0].pageY
-                    e.preventDefault()
-                }
-            })
-            questionAndRepWrapper.addEventListener("touchmove", (e) => {
-                if (!mov) {
-                    distY = e.changedTouches[0].pageY - startY
-                    swipedir = (distY < 0) ? 'up' : 'down'
-                    appearRep(swipedir, distY)
-                    e.preventDefault()
-                }
-            })
+            */
+function load_element_slide() {
 
-            questionAndRepWrapper.addEventListener('touchend', (e) => {
+    console.log("recup Element")
+    const waitingWrapper = document.querySelector("#waiting-wrapper")
+    const questionAndRepWrapper = document.querySelector("#questionAndRep-wrapper")
+    const previewRep1 = document.querySelector("#previewRep1")
+    const previewRep2 = document.querySelector("#previewRep2")
+    console.log("inside timeout");
+    menu.style = "opacity : 0;";
+    waitingWrapper.style = "display : none"
+    questionAndRepWrapper.style = "display : flex;"
+    let swipedir, startY, distY;
 
-                if (!mov) {
-                    distY = e.changedTouches[0].pageY - startY
-                    swipedir = (distY < 0) ? 'up' : 'down'
-                    appearRep(0, 0);
-                    if (Math.abs(distY) >= 300) {
-                        swipe(swipedir)
-                    }
+    questionAndRepWrapper.addEventListener('touchstart', (e) => {
+        if (!mov) {
+            swipedir = 'none'
+            distY = 0
+            startY = e.changedTouches[0].pageY
+            e.preventDefault()
+        }
+    })
+    questionAndRepWrapper.addEventListener("touchmove", (e) => {
+        if (!mov) {
+            distY = e.changedTouches[0].pageY - startY
+            swipedir = (distY < 0) ? 'up' : 'down'
+            appearRep(swipedir, distY)
+            e.preventDefault()
+        }
+    })
 
-                }
+    questionAndRepWrapper.addEventListener('touchend', (e) => {
 
-            })
-
-            function swipe(dir) {
-                mov = true
-                window.navigator.vibrate(200);
-                setTimeout(function () {
-                    menu.style = "opacity : 1; transition : 1s";
-                }, 750);
-                if (dir === "up") {
-                    questionAndRepWrapper.style = "display : flex;top:-200vh"
-                    postMood(2,idResponseN)
-                } else {
-                    questionAndRepWrapper.style = "display : flex;top:0"
-                    postMood(1,idResponseP)
-                }
+        if (!mov) {
+            distY = e.changedTouches[0].pageY - startY
+            swipedir = (distY < 0) ? 'up' : 'down'
+            appearRep(0, 0);
+            if (Math.abs(distY) >= 300) {
+                swipe(swipedir)
             }
 
-            function appearRep(dir, distY) {
-                if (distY > 100) {
-                    distY = 100;
-                }
-                if (distY < -100) {
-                    distY = -100;
-                }
-                if (dir === "down") {
-                    previewRep1.style = `opacity : 1; top:${-100 + distY}px`
-                    previewRep2.style = `opacity : 0; bottom:-100px`
-                } else if (dir === "up") {
-                    previewRep2.style = `opacity : 1; bottom:${-100 - distY}px`
-                    previewRep1.style = `opacity : 0; top:-100px`
-                } else {
+        }
 
-                    previewRep1.style = `opacity : 0; top:-100px`
-                    previewRep2.style = `opacity : 0; bottom:-100px`
-                }
-            }
-        }, 2000);
+    })
+
+    function swipe(dir) {
+        mov = true
+        window.navigator.vibrate(200);
+        setTimeout(function () {
+            menu.style = "opacity : 1; transition : 1s";
+        }, 750);
+        if (dir === "up") {
+            questionAndRepWrapper.style = "display : flex;top:-200vh"
+            postMood(2, idResponseN)
+        } else {
+            questionAndRepWrapper.style = "display : flex;top:0"
+            postMood(1, idResponseP)
+        }
     }
-})
+
+    function appearRep(dir, distY) {
+        if (distY > 100) {
+            distY = 100;
+        }
+        if (distY < -100) {
+            distY = -100;
+        }
+        if (dir === "down") {
+            previewRep1.style = `opacity : 1; top:${-100 + distY}px`
+            previewRep2.style = `opacity : 0; bottom:-100px`
+        } else if (dir === "up") {
+            previewRep2.style = `opacity : 1; bottom:${-100 - distY}px`
+            previewRep1.style = `opacity : 0; top:-100px`
+        } else {
+
+            previewRep1.style = `opacity : 0; top:-100px`
+            previewRep2.style = `opacity : 0; bottom:-100px`
+        }
+    }
+}
+            /*
+        }, 0);
+    }
+})*/
 
 menu.onclick = () => {
     console.log("test")
