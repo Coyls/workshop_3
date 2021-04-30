7c9e97eb0d5a467209d55321a02b5123ab920803
Coyls <76538870+Coyls@users.noreply.github.com>
2021-04-30 10:28:40 +0200

    anim load

--- public/mobile/js/onload_animation.js
+++ public/mobile/js/onload_animation.js
@@ -1,10 +1,23 @@
-const logo = document.getElementById("onload_animation")
+const logo = document.querySelector(".onload_animation")
 
-    let animItem = bodymovin.loadAnimation({
-        wrapper: logo,
-        animType: 'svg',
-        loop: false,
-        path: "./image/onload_animation.json"
-    });
+let animItem = bodymovin.loadAnimation({
+    wrapper: logo,
+    animType: 'svg',
+    loop: false,
+    path: "./image/onload_animation.json"
+});
 
-    
+const waitOnload = (delay) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, delay);
+    })
+}
+
+waitOnload(3333).then(() => {
+    logo.classList.add("noneOnload");
+})
+
+
+
