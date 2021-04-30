9148b2d91a3496bc276f6cf97c51d9d18456900f
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2021-04-30 08:59:33 +0200

    dbzhudzgxbzbayavsxa

--- public/mobile/js/about.js
+++
@@ -1,8 +0,0 @@
-const instagramWrapper = document.querySelector("#instagram-wrapper-part-two")
-const projectNumber = 6
-
-for (let i = 0; i < projectNumber; i++) {
-    const imageWrapper = document.createElement("div")
-    imageWrapper.classList.add("imageWrapper")
-    instagramWrapper.append(imageWrapper)
-}
